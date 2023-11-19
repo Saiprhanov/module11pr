448b948b78041a5dc5d1ce9a53890d2b4cc2d56d
Orken <Orken@DESKTOP-26SJ0GV>
2023-11-19 19:29:55 +0600

    Добавьте файлы проекта.

---
+++ ClassLibrary1/employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public struct Employee
+    {
+        public string Name;
+        public Vacancies Vacancy;
+        public int[] HireDate;
+        public int Salary;
+
+        public string GetFullInfo()
+        {
+            string hireDateString = $"{HireDate[0]}-{HireDate[1]}-{HireDate[2]}";
+            return $"Name: {Name}, Vacancy: {Vacancy}, Hire Date: {hireDateString}, Salary: {Salary}";
+        }
+    }
+}
